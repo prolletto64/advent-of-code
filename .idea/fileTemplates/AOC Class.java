0cc9756700ba8d888cb79c008c166d1cbd7a4242
prolletto64 <poiluonzo@mail.com>
2023-12-12 15:34:15 +0100

    template correction

--- .idea/fileTemplates/AOC Class.java
+++ .idea/fileTemplates/AOC Class.java
@@ -1,6 +1,7 @@
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
 #parse("File Header.java")
 public class ${NAME} {
+    private static String[] lines = readLines("${NAME}");
     public static void run(){
         
     }
