70d12373e66904222bda90ab17cf987f8d8d36d7
prolletto64 <poiluonzo@mail.com>
2023-12-12 14:55:04 +0100

    day one setup

---
+++ src/main/java/it/prolletto64/AOC/Main.java
@@ -0,0 +1,9 @@
+package it.prolletto64.AOC;
+
+import it.prolletto64.AOC.twentythree.one;
+
+public class Main {
+    public static void main(String[] args) {
+        one.run();
+    }
+}
---
+++ src/main/java/it/prolletto64/AOC/twentythree/one.java
@@ -0,0 +1,7 @@
+package it.prolletto64.AOC.twentythree;
+
+public class one {
+    public static void run(){
+
+    }
+}
