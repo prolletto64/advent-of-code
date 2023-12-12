c22d695d985ef3de91e1e1f14670fd87b11029a0
prolletto64 <poiluonzo@mail.com>
2023-12-12 17:53:28 +0100

    day two part one

--- src/main/java/it/prolletto64/AOC/Main.java
+++ src/main/java/it/prolletto64/AOC/Main.java
@@ -1,9 +1,10 @@
 package it.prolletto64.AOC;
 
 import it.prolletto64.AOC.twentythree.one;
+import it.prolletto64.AOC.twentythree.two;
 
 public class Main {
     public static void main(String[] args) {
-        one.run();
+        two.run();
     }
 }
--- src/main/java/it/prolletto64/AOC/twentythree/two.java
+++ src/main/java/it/prolletto64/AOC/twentythree/two.java
@@ -1,11 +1,33 @@
 package it.prolletto64.AOC.twentythree;
 
+import java.util.Map;
+
 import static it.prolletto64.AOC.utils.inputHandler.readLines;
 
 public class two {
     private static String[] lines = readLines("two");
 
-    public static void run() {
+    private static final Map<String,Integer> CUBES = Map.of(
+            "red", 12,
+            "green", 13,
+            "blue", 14
+    );
 
+    public static void run() {
+        int total=0;
+        boolean invalid;
+        for(String line: lines){
+            invalid=false;
+            for (String el : line.split(":")[1].split(",")) {
+                for (String e : el.split(";")) {
+                    e=e.substring(1);
+                    if(Integer.parseInt(e.split(" ")[0])>CUBES.get(e.split(" ")[1])){
+                        invalid=true;
+                    }
+                }
+            }
+            if(!invalid) total+=Integer.parseInt(line.split(":")[0].split(" ")[1]);
+        }
+        System.out.println(total);
     }
 }
