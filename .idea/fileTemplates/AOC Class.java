2ed7b1b2717a686dccbb4377dde553da5fe0640a
prolletto64 <poiluonzo@mail.com>
2023-12-12 17:00:35 +0100

    day two template

--- .idea/fileTemplates/AOC Class.java
+++ .idea/fileTemplates/AOC Class.java
@@ -1,4 +1,6 @@
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+
+import static it.prolletto64.AOC.utils.inputHandler.readLines;
 #parse("File Header.java")
 public class ${NAME} {
     private static String[] lines = readLines("${NAME}");
---
+++ src/main/java/it/prolletto64/AOC/twentythree/two.java
@@ -0,0 +1,11 @@
+package it.prolletto64.AOC.twentythree;
+
+import static it.prolletto64.AOC.utils.inputHandler.readLines;
+
+public class two {
+    private static String[] lines = readLines("two");
+
+    public static void run() {
+
+    }
+}
