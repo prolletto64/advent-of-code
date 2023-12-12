e572d84e879bbd563d52c56ca587b1ac296fd925
prolletto64 <poiluonzo@mail.com>
2023-12-12 15:34:27 +0100

    day one part one

--- src/main/java/it/prolletto64/AOC/twentythree/one.java
+++ src/main/java/it/prolletto64/AOC/twentythree/one.java
@@ -1,7 +1,30 @@
 package it.prolletto64.AOC.twentythree;
 
-public class one {
-    public static void run(){
+import static it.prolletto64.AOC.utils.inputHandler.readLines;
 
+public class one {
+    private static final String[] lines = readLines("one");
+    public static void run(){
+        int total=0;
+        int firstDigit;
+        int lastDigit;
+        int digit;
+        for (String line:lines) {
+            firstDigit=0;
+            lastDigit=0;
+            for (char c: line.toCharArray()) {
+                digit=-1;
+                try{
+                    digit=Integer.parseInt(String.valueOf(c));
+                }catch (Exception e){}
+                if(digit!=-1){
+                    if(firstDigit==0)
+                        firstDigit=digit;
+                    lastDigit=digit;
+                }
+            }
+            total+=firstDigit*10+lastDigit;
+        }
+        System.out.println(total);
     }
 }
